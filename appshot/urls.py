c1ea083cd6b2786aeab0e4c6986832c61f3ed366
jaspreet <jaspreetsangha204@yahoo.com>
2023-05-31 14:50:16 -0700

    Feature 9 Complete

--- appshot/urls.py
+++ appshot/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from appshot.views import todo_list, todo_list_detail
+from appshot.views import todo_list, todo_list_detail, todo_list_create
 
 app_name = "todos"
 
 urlpatterns = [
     path("", todo_list, name="todo_list_list"),
     path("<int:id>/", todo_list_detail, name="todo_list_detail"),
+    path("create/", todo_list_create, name="todo_list_create"),
 ]
--- appshot/views.py
+++ appshot/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from appshot.models import TodoList, TodoItem
+from appshot.models import TodoList
+from appshot.forms import TodoListForm
 
 # Create your views here.
 
@@ -12,6 +13,18 @@ def todo_list(request):
 
 def todo_list_detail(request, id):
     todo_list = get_object_or_404(TodoList, id=id)
-    tasks = TodoItem.objects.filter(list=todo_list)
+    tasks = todo_list.items.all()
     context = {"todo_list": todo_list, "tasks": tasks}
     return render(request, "todos/todo_list_detail.html", context)
+
+
+def todo_list_create(request):
+    if request.method == "POST":
+        form = TodoListForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("todos:todo_list_list")
+    else:
+        form = TodoListForm()
+        context = {"form": form}
+        return render(request, "todos/todo_list_create.html", context)
